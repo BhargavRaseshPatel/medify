1d16ee73e29ee68fa8905fc50685b6b3755c74f5
Bhargav Patel <bhargavrpatelcanada@gmail.com>
2025-09-12 12:12:18 -0400

    medical card implemented

--- src/components/MedicalCenterCard.jsx
+++ src/components/MedicalCenterCard.jsx
@@ -13,20 +13,23 @@ const MedicalCenterCard = ({ data }) => {
             <div style={{ width: '487px', minWidth: '487px', height: '220px', display: 'flex', alignItems: 'center', marginLeft: "24px" }}>
                 <img src='medicalcenter.svg' style={{ width: '140px', height: "140px" }} />
 
-                <div style={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', marginLeft: '24px', gap :'18px' }}>
+                <div style={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', marginLeft: '24px', gap: '18px', marginTop : '24px' }}>
                     <p style={{ color: '#14BEF0', fontSize: '20px', whiteSpace: 'nowrap' }}>{data['Hospital Name'].toLowerCase()
                         .split(' ')
                         .map(word => word.charAt(0).toUpperCase() + word.slice(1))
                         .join(' ')}</p>
                     <div>
-                        <p style={{ fontWeight: 'bolder' }}>{data['State']}, {data['City']}</p>
+                        <p style={{ fontWeight: 'bolder' }}>{data['State']}, {data['City'].charAt(0) + data['City'].slice(1).toLowerCase()}</p>
                         <p style={{ color: '#414146', fontSize: '14px' }}>Smilessence Center for Advanced Dentistry + 1 more</p>
                     </div>
 
-                    <div style={{display : 'flex'}}>
+                    <div style={{ display: 'flex' }}>
+                        <p style={{ color: '#02A401', fontWeight: 'bolder', fontSize: '14px' }}>FREE</p>
+                        <p style={{ color: '#414146' }}><span style={{ marginLeft: '8px', textDecoration: 'line-through' }}> ₹500</span> Consultation fee at clinic</p>
+                    </div>
 
-                    <p style={{ color: '#02A401', fontWeight: 'bolder', fontSize: '14px' }}>FREE</p>
-                    <p style={{color :'#414146'}}><span style={{marginLeft : '8px', textDecoration: 'line-through'}}> ₹500</span> Consultation fee at clinic</p>
+                    <div style={{backgroundColor : '#00A500', color : 'white', width : '44px', height : '22px', borderRadius : '3px', display : 'flex' , justifyContent : 'center', alignItems : 'center', gap: '4px'}}>
+                        <img src='thumbsUp.svg'/>{data['Hospital overall rating']}
                     </div>
                 </div>
 
--- src/screen/FindDoctor.jsx
+++ src/screen/FindDoctor.jsx
@@ -46,9 +46,7 @@ const FindDoctor = () => {
 
     return (
         <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: "center", backgroundColor: '#E7F0FF' }}>
-
-
-            <div style={{ width: '1166px', display: 'flex', gap: '24px', border: '1px solid #F0F0F0', borderRadius: '15px', justifyContent: 'center', alignItems: 'center' }}>
+            <div style={{ width: '1166px', display: 'flex', gap: '24px', border: '1px solid #F0F0F0', borderRadius: '15px', justifyContent: 'center', alignItems: 'center', backgroundColor: 'white', marginTop: '48px' }}>
                 <div style={{ height: '111px', display: 'flex', alignItems: 'center', gap: '24px', color: '#9CA3AF' }}>
                     <div style={{ width: "326px", height: '50px', border: '1px solid #F0F0F0', backgroundColor: '#FAFBFE', padding: '8px', borderRadius: '8px', display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
                         <div size={{ width: "18px", height: '18px', padding: '4px' }}>
@@ -85,10 +83,23 @@ const FindDoctor = () => {
 
             </div >
 
-            <div style={{display : 'flex' , flexDirection : 'column', gap : '24px'}}>
-                {medicalCenter && medicalCenter.map((data, index) => (
-                    (<MedicalCenterCard key={index} data={data} />)
-                ))}
+            <div>
+                <div style={{ display: 'flex', width: '100%', flexDirection: 'column', justifyContent: 'flex-start', gap: '24px', marginTop: '48px' }}>
+                    {medicalCenter.length > 0 && <p style={{ fontSize: '24px' }}>{medicalCenter?.length} medical centers available in {selectedCities.charAt(0).toUpperCase() + selectedCities.slice(1).toLowerCase()}</p>}
+
+                    <div style={{ display: 'flex', gap: '16px' }}>
+                        {medicalCenter.length > 0 &&
+                            <div style={{display : 'flex',gap: '24px' }}>
+                                <div style={{ display: 'flex', flexDirection : 'column',gap: '24px'  }}>
+                                    {medicalCenter && (medicalCenter.map((data, index) => (
+                                        (<MedicalCenterCard key={index} data={data} />)
+                                    )))}
+                                </div>
+                                <img src='side-image.svg' width={'363px'} height={'268px'} />
+                            </div>
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     )
