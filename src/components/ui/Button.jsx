6f8ecc7dc028fea4136cc7ddc32aad7858a44c08
Bhargav Patel <bhargavrpatelcanada@gmail.com>
2025-09-12 08:59:21 -0400

    correct error

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = ({ name, onClick, id = none }) => {
+const Button = ({ name, onClick, id = 'abc' }) => {
     return (
         <div>
             <button id={id} style={{color : 'white' , padding : '9px', paddingRight : '24px', paddingLeft : '24px', backgroundColor : '#2AA8FF', border : '0px', borderRadius : '8px'}}
