adca011469d5a815d83dead627a5a4a82695cb93
Bhargav Patel <bhargavrpatelcanada@gmail.com>
2025-09-15 17:32:31 -0400

    remove error

--- src/components/HomeComponents/HomeSearch.jsx
+++ src/components/HomeComponents/HomeSearch.jsx
@@ -1,18 +1,88 @@
 import { FaSearch } from "react-icons/fa";
 import Button from "../ui/Button";
 import InputField from "../ui/InputField";
+import { useEffect, useState } from "react";
+import { IoLocationOutline } from "react-icons/io5";
+
 
 const HomeSearch = () => {
+
+        const [states, setStates] = useState([])
+            const [cities, setCities] = useState([])
+            const [selectedState, setSelectedState] = useState('')
+            const [selectedCities, setSelectedCities] = useState('')
+            const [medicalCenter, setMedicalCenter] = useState([])
+        
+            useEffect(() => {
+                const fetchStates = async () => {
+                    const states = await (await fetch('https://meddata-backend.onrender.com/states')).json();
+                    setStates(states);
+                };
+                fetchStates();
+            })
+        
+            const handleState = (event) => {
+                const fetchCities = async () => {
+                    const cities = await (await fetch(`https://meddata-backend.onrender.com/cities/${event.target.value}`)).json();
+                    setCities(cities);
+                };
+                setSelectedState(event.target.value);
+                fetchCities();
+            }
+        
+            const handleCities = (event) => {
+                setSelectedCities(event.target.value)
+            }
+        
+            const searchMedicalCenters = () => {
+                const fetchMedicalCenters = async () => {
+                    try {
+                        const response = await fetch(`https://meddata-backend.onrender.com/data?state=${selectedState}&city=${selectedCities}`);
+                        const data = await response.json();
+                        setMedicalCenter(data);
+                    } catch (error) {
+                        console.error("Error fetching medical centers:", error);
+                    }
+                };
+                fetchMedicalCenters();
+            }
         return (
-                <div style={{ background: 'white', borderRadius: '16px', display: 'flex', padding: '48px', justifyContent :'space-between', zIndex: '2', paddingTop :'0px' }}>
-                        <div style={{display : 'flex',width : '100%', justifyContent: 'space-around' }}>
-                                <InputField icon={'Search'} placeholder={'State'} width={'285px'} id={'state'}/>
-                                <InputField width={'285px'} icon={'Search'} placeholder={'City'} id={'city'}/>
+                <div style={{ width: '1166px', display: 'flex', gap: '24px', border: '1px solid #F0F0F0', borderRadius: '15px', justifyContent: 'center', alignItems: 'center', backgroundColor: 'white', marginTop: '48px' }}>
+                <div style={{ height: '111px', display: 'flex', alignItems: 'center', gap: '24px', color: '#9CA3AF' }}>
+                    <div style={{ width: "326px", height: '50px', border: '1px solid #F0F0F0', backgroundColor: '#FAFBFE', padding: '8px', borderRadius: '8px', display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
+                        <div size={{ width: "18px", height: '18px', padding: '4px' }}>
+                            <IoLocationOutline style={{ fontSize: '32px' }} />
                         </div>
-                        <div>
-                                <Button name={'Search'} />
+
+                        <div style={{ width: '100%', display: 'flex', alignItems: 'center', marginLeft: '8px' }} id='state'>
+                            <select style={{ width: "100%", border: '0px', fontSize: '14px', color: '#ABB6C7' }} onChange={handleState} id='state'>
+                                {states.map((data, index) => (
+                                    <option key={index}>{data}</option>
+                                ))}
+                            </select>
                         </div>
+                    </div>
                 </div>
+
+                <div style={{ height: '111px', display: 'flex', alignItems: 'center', gap: '24px', color: '#9CA3AF' }} id='city'>
+                    <div style={{ width: "522px", height: '50px', border: '1px solid #F0F0F0', backgroundColor: '#FAFBFE', padding: '8px', borderRadius: '8px', display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
+                        <div size={{ width: "18px", height: '18px', padding: '4px' }}>
+                            <IoLocationOutline style={{ fontSize: '32px' }} />
+                        </div>
+
+                        <div style={{ width: '100%', display: 'flex', alignItems: 'center', marginLeft: '8px' }}>
+                            <select style={{ width: "100%", border: '0px', fontSize: '14px', color: '#ABB6C7' }} id='city' onChange={handleCities}>
+                                {cities.map((data, index) => (
+                                    <option key={index}>{data}</option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <Button id={'searchBtn'} name={'Search'} onClick={searchMedicalCenters} />
+
+            </div >
         )
 }
 
