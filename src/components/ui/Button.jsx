ccb8b35a4aac34546add84f671ffd3954bb1bc41
Bhargav Patel <bhargavrpatelcanada@gmail.com>
2025-09-19 12:26:09 -0400

    modify navigation

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 
-const Button = ({ name, onClick, id = 'abc', ...props }) => {
-    return (
-        <div>
-            <button id={id} style={{color : 'white' , padding : '9px', paddingRight : '24px', paddingLeft : '24px', backgroundColor : '#2AA8FF', border : '0px', borderRadius : '8px' , ...props}}
-            onClick={onClick}>
-                {name}
-            </button>
-        </div>
-    )
+const Button = ({ name, onClick, id = 'abc', style = {}, ...props }) => {
+  return (
+    <div>
+      <button
+        id={id}
+        style={{
+          color: 'white',
+          padding: '9px',
+          paddingRight: '24px',
+          paddingLeft: '24px',
+          backgroundColor: '#2AA8FF',
+          border: '0px',
+          borderRadius: '8px',
+          ...style, // ✅ merge custom styles safely
+        }}
+        onClick={onClick}
+        {...props} // ✅ spread props correctly here
+      >
+        {name}
+      </button>
+    </div>
+  )
 }
 
 export default Button
